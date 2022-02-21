63935dc90f6791f5d6150c6dbd62ac8ee6b30459
Roman Kolesnikov <krn@pinroot.org>
2022-02-21 16:40:28 +0700

    init

---
+++ app.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+const hostname = '0.0.0.0';
+const port = 3000;
+
+const crypto = require("crypto");
+
+const id = crypto.randomBytes(16).toString("hex");
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end(`Hello World! ${id}`);
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
