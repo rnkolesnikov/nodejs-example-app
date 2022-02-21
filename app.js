d01d9b949224120723f6a69d2ea75355ab1256b3
Roman Kolesnikov <krn@pinroot.org>
2022-02-21 16:54:59 +0700

    Update

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const hostname = '0.0.0.0';
 const port = 3000;
 
 const crypto = require("crypto");
-
 const id = crypto.randomBytes(16).toString("hex");
+
+const version = "v0.0.2";
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end(`Hello World! ${id}`);
+  res.end(`Node Example App ${version}, id: ${id}`);
 });
 
 server.listen(port, hostname, () => {
